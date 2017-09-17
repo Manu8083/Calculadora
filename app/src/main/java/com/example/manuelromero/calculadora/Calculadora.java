5088065171bf0a2b3e070103deb13bee2af44558
Manuel Romero <manuelromero@MacBook-Pro-de-Manuel.local>
2017-09-17 12:57:05 -0500

    Funcionalida basica, con errores en la suma

--- app/src/main/java/com/example/manuelromero/calculadora/Calculadora.java
+++ app/src/main/java/com/example/manuelromero/calculadora/Calculadora.java
@@ -2,13 +2,36 @@ package com.example.manuelromero.calculadora;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.*;
+import android.view.View.OnClickListener;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class Calculadora extends AppCompatActivity {
+
+public class Calculadora extends AppCompatActivity implements OnClickListener{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculadora);
+        View boton = findViewById(R.id.BotonSumar);
+        boton.setOnClickListener(this);
+        //recomendado por video boton.setOnClickListener(this);
+    }
+
+    public void onClick(View vista){
+        TextView temp;
+        temp = (TextView) findViewById(R.id.Num1);
+        TextView resultado = (TextView)findViewById(R.id.ViewResultado);
+        try{
+            float a = Float.parseFloat(temp.getText().toString());
+            temp = (TextView) findViewById(R.id.Num2);
+            float b = Float.parseFloat(temp.getText().toString());
+            resultado.setText("Resultado : "+ (a + b));
+        }catch (Exception e){};
+
+            resultado.setText("Los datos solo deben ser numeros");
+
     }
 
 }
