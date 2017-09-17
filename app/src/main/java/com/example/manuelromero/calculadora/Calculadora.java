001f1dc1d4dd07f08dd53cd8989e0e7ac0d65a77
Carlos Andres Martinez <tizam15@hotmail.com>
2017-09-17 15:24:39 -0400

    Agregado funcion restar

--- app/src/main/java/com/example/manuelromero/calculadora/Calculadora.java
+++ app/src/main/java/com/example/manuelromero/calculadora/Calculadora.java
@@ -53,4 +53,21 @@ public class Calculadora extends AppCompatActivity implements OnClickListener{
         };
     }
 
+    public void Restar(View v){
+        EditText oper1, oper2;
+        oper1 = (EditText)findViewById(R.id.Num1); // se obtiene los datos del usuario
+        oper2 = (EditText)findViewById(R.id.Num2);
+
+        TextView resultado = (TextView)findViewById(R.id.ViewResultado); // aqui se mostrar el resultado
+        try{
+
+            int a = Integer.parseInt(oper1.getText().toString()); //los datos agregador por el usuario se convierten en interger para poder sumar
+            int b = Integer.parseInt(oper2.getText().toString()); //los datos agregador por el usuario se convierten en interger para poder sumar
+
+            resultado.setText("Resultado : " + (a - b)); //Se imprime el resultado
+        }catch (Exception e){
+            resultado.setText("Los datos solo deben ser numeros"); //en caso de error se imprime esto
+        };
+    }
+
 }
